4d06a4fb5cb7d454a504001ab6451f63f5d92efa
Phomolo Ntokoane <ntokoanephomolo76@gmail.com>
2023-10-23 14:03:05 +0200

    Used nextui to recreate navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,65 +1,67 @@
 "use client";
 
 import Link from "next/link";
-import { ChangeEvent, useEffect, useState } from "react";
-import Drawer from "react-modern-drawer";
-import { HiMenu, HiX } from "react-icons/hi";
-
-import 'react-modern-drawer/dist/index.css'
+import { useState } from "react";
+import { HiSearch } from "react-icons/hi";
+import {
+  Input,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
 
 function NavBar() {
-  // const [currentTheme, setTheme] = useState<"dark" | "light">("light");
-  const [drawerOpen, setDrawer] = useState(false);
-
-  // useEffect(() => {
-  //   const documentClassList = document.documentElement.classList;
-  //   const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  //   if (isDark) documentClassList.add("dark");
-  //   else documentClassList.remove("dark");
-  // }, []);
-
-  // const handleThemeChange = () => {
-  //   const documentClassList = document.documentElement.classList;
-  //   setTheme((prev) => (prev === "dark" ? "light" : "dark"));
-
-  //   if (currentTheme === "dark") {
-  //     documentClassList.add("dark");
-  //   } else if (currentTheme === "light") {
-  //     documentClassList.remove("dark");
-  //   }
-  // };
-
-  const closeDrawer = () => setDrawer(false);
-  const openDrawer = () => setDrawer(true);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const items = [
+    { name: "Bake", url: "#" },
+    { name: "Ice cream", url: "#" },
+    { name: "Sweets", url: "#" },
+  ];
 
   return (
-    <>
-      <nav className="mb-2 flex items-center gap-4 p-3 md:justify-between xl:mx-auto xl:w-[1200px]">
-        <button
-          className="rounded p-2 outline outline-primary md:hidden"
-          onClick={openDrawer}
-        >
-          <HiMenu className="h-[18px] w-[18px] text-primary" />
-        </button>
+    <Navbar onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="text-primary md:hidden"
+        />
+        <NavbarBrand>
+          <Link href="/">
+            <h1 className="text-2xl font-bold text-primary">Roman Scoops</h1>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
 
-        <Link href="/">
-          <h1 className="text-2xl font-bold text-primary">Roman Scoops</h1>
-        </Link>
+      <NavbarContent className="max-md:hidden" justify="center">
+        {items.map((i, num) => (
+          <NavbarItem key={num}>
+            <Link className="font-semibold text-primary" href={i.url}>
+              {i.name}
+            </Link>
+          </NavbarItem>
+        ))}
+      </NavbarContent>
 
-        <div className="max-md:ml-auto max-md:w-1/3">
-          <input className="rounded p-2 outline outline-accent max-md:w-full" type="text" />
-        </div>
-      </nav>
-
-      <Drawer open={drawerOpen} onClose={closeDrawer} direction={"left"}>
-        <section className="p-4 flex flex-col">
-          <button className="my-3" onClick={closeDrawer}>
-            <HiX className="w-[24px] h-[24px] text-primary"/>
-          </button>
-          stuff
-        </section>
-      </Drawer>
-    </>
+      <NavbarContent justify="end">
+        <Input
+          className="max-w-[200px] rounded outline outline-accent"
+          startContent={<HiSearch className="h-[18px] w-[18px] text-accent" />}
+        />
+      </NavbarContent>
+      <NavbarMenu className="bg-background">
+        {items.map((i, num) => (
+          <NavbarMenuItem key={num}>
+            <Link className="font-semibold text-primary" href={i.url}>
+              {i.name}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
 
