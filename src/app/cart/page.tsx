38a9295e0661591b59f2c6728d5937ca816fa168
Phomolo Ntokoane <105977946+vandaminator@users.noreply.github.com>
2023-12-03 19:42:05 +0000

    Update 3 files

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,20 +1,29 @@
 "use client"
 import {useContext} from "react"
 import { cartContext } from "@/context/cart";
-import {Button, Image} from "@nextui-org/react"
+import {Button, Image, Divider} from "@nextui-org/react"
+import { useSession } from "next-auth/react"
 
 function CartPage() {
     const {cart, removeItemCart} = useContext(cartContext)
+    const {data: session, status} = useSession()
+
+    console.log(status)
+
     return <>
-    <div className="flex flex-col gap-3">
+    <h1 className="text-3xl text-primary font-semibold">Cart</h1>
+    <Divider />
+    <div className="mt-4 flex flex-col gap-3">
         {cart.map((p) => (
             <div className="grid grid-cols-3 justify-between items-center" key={p.id}>
                 <Button className="bg-red-600 p-3 rounded text-white w-fit" onClick={() => removeItemCart(p.id)}>X</Button>
                 <p className="text-text">{p.title}</p>
                 <Image className="w-[100px] h-[100px]" src={p.image} alt="Product"/>
-                <div className="flex">
-                    <p className="text-accent">Price: {p.price}</p>
-                </div>
+                <p className="text-accent">Price: R{p.price}</p>
+                {/* <div className="flex gap-3">
+                    <label htmlFor=""></label>
+                    <input type="text" />
+                </div> */}
             </div>
         ))}
     </div>
--- src/components/Products/ProductCard.tsx
+++ src/components/Products/ProductCard.tsx
@@ -15,7 +15,7 @@ const ProductCard = ({ item: { id, title, price, image } }: Props) => {
       <CardFooter className="flex-col">
         <div className="flex w-full justify-between">
           <p className="text-primary">{title}</p>
-          <p className="text-accent">{price.toLocaleString()}</p>
+          <p className="text-accent">R{price.toLocaleString()}</p>
         </div>
         <Button className="bg-accent w-full text-white font-bold" href={`/${id}`} as={Link}>Buy</Button>
       </CardFooter>
